2efa56aa7162c388ed1c9c04e2097a16d563ab66
Aditya Digambar Tarar <aditya.tarar@e-zest.in>
2022-04-17 16:29:10 +0530

    second commit

--- client/post/PostList.js
+++ client/post/PostList.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Post from './Post'
+import React from "react";
+import PropTypes from "prop-types";
+import Post from "./Post";
+import auth from "./../auth/auth-helper";
 
-export default function PostList (props) {
-    return (
-      <div style={{marginTop: '24px'}}>
-        {props.posts.map((item, i) => {
-            return <Post post={item} key={i} onRemove={props.removeUpdate}/>
-          })
-        }
-      </div>
-    )
+export default function PostList(props) {
+  console.log("props", props.sentimentScore);
+  const jwt = auth.isAuthenticated();
+  // console.log(jwt.user.name);
+  return (
+    <div style={{ marginTop: "24px" }}>
+      {props.posts.map((item, i) => {
+        return (
+          <Post
+            post={item}
+            key={i}
+            onRemove={props.removeUpdate}
+            user={jwt.user.name}
+          />
+        );
+      })}
+    </div>
+  );
 }
 PostList.propTypes = {
   posts: PropTypes.array.isRequired,
-  removeUpdate: PropTypes.func.isRequired
-}
+  removeUpdate: PropTypes.func.isRequired,
+};
--- client/user/ProfileData.js
+++ client/user/ProfileData.js
@@ -1,142 +1,143 @@
-import React, { useState, useEffect } from 'react'
-import {makeStyles} from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction' 
-import ListItemText from '@material-ui/core/ListItemText' 
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import Edit from '@material-ui/icons/Edit'
-import Divider from '@material-ui/core/Divider'
-import DeleteUser from './DeleteUser'
-import auth from './../auth/auth-helper'
-import {read} from './api-user.js'
-import {Redirect, Link} from 'react-router-dom'
-import FollowProfileButton from './../user/FollowProfileButton'
-import ProfileTabs from './../user/ProfileTabs'
-import {listByUser} from './../post/api-post.js'
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Edit from "@material-ui/icons/Edit";
+import Divider from "@material-ui/core/Divider";
+import DeleteUser from "./DeleteUser";
+import auth from "./../auth/auth-helper";
+import { read } from "./api-user.js";
+import { Redirect, Link } from "react-router-dom";
+import FollowProfileButton from "./../user/FollowProfileButton";
+import ProfileTabs from "./../user/ProfileTabs";
+import { listByUser } from "./../post/api-post.js";
 
-import Sentiment from 'sentiment';
+import Sentiment from "sentiment";
 const sentiment = new Sentiment();
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: theme.mixins.gutters({
     maxWidth: 600,
-    margin: 'auto',
+    margin: "auto",
     padding: theme.spacing(3),
-    marginTop: theme.spacing(5)
+    marginTop: theme.spacing(5),
   }),
   title: {
     margin: `${theme.spacing(2)}px ${theme.spacing(1)}px 0`,
     color: theme.palette.protectedTitle,
-    fontSize: '1em'
+    fontSize: "1em",
   },
   bigAvatar: {
     width: 60,
     height: 60,
-    margin: 10
-  }
-}))
+    margin: 10,
+  },
+}));
 
 export default function ProfileData({ match }) {
-  const classes = useStyles()
+  const classes = useStyles();
   const [values, setValues] = useState({
-    user: {following:[], followers:[]},
+    user: { following: [], followers: [] },
     redirectToSignin: false,
-    following: false
-  })
-  const [posts, setPosts] = useState([])
-  const jwt = auth.isAuthenticated()
+    following: false,
+  });
+  const [posts, setPosts] = useState([]);
+  const jwt = auth.isAuthenticated();
 
   useEffect(() => {
-    const abortController = new AbortController()
-    const signal = abortController.signal
-  
-    read({
-      userId: match.params.userId
-    }, {t: jwt.token}, signal).then((data) => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    read(
+      {
+        userId: match.params.userId,
+      },
+      { t: jwt.token },
+      signal
+    ).then((data) => {
       if (data && data.error) {
-        setValues({...values, redirectToSignin: true})
+        setValues({ ...values, redirectToSignin: true });
       } else {
-        let following = checkFollow(data)
-        setValues({...values, user: data, following: following})
-        loadPosts(data._id)
+        let following = checkFollow(data);
+        setValues({ ...values, user: data, following: following });
+        loadPosts(data._id);
       }
-    })
-    return function cleanup(){
-      abortController.abort()
-    }
+    });
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, [match.params.userId]);
 
-  }, [match.params.userId])
-  
   const checkFollow = (user) => {
-    const match = user.followers.some((follower)=> {
-      return follower._id == jwt.user._id
-    })
-    return match
-  }
+    const match = user.followers.some((follower) => {
+      return follower._id == jwt.user._id;
+    });
+    return match;
+  };
   const loadPosts = (user) => {
-    listByUser({
-      userId: user
-    }, {
-      t: jwt.token
-    }).then((data) => {
-      if (data.error) {
-        console.log(data.error)
-      } else {
-        setPosts(data)
-         {
-             data.map((user) => (
-            setPhrase(user.text)
-      ))}
+    listByUser(
+      {
+        userId: user,
+      },
+      {
+        t: jwt.token,
       }
-    })
-  }
-
-    const photoUrl = values.user._id
-              ? `/api/users/photo/${values.user._id}?${new Date().getTime()}`
-              : '/api/users/defaultphoto'
-    if (values.redirectToSignin) {
-      return <Redirect to='/signin'/>
-    }
-  const [phrase, setPhrase] = useState('');
-  const [sentimentScore, setSentimentScore] = useState('');
-  
-  useEffect(() => {
-    setSentimentScore((sentiment.analyze(phrase))) ;
-  }, [phrase]);
-console.log(sentimentScore)
-    return (
-      <Paper className={classes.root} elevation={4}>
-        <div>
-
-             {
-          sentimentScore !== null ?
-            <h1>Sentiment Analysis Score: {sentimentScore.score}</h1>
-            : ''
-        }
-
+    ).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setPosts(data);
         {
-          sentimentScore ?
-            sentimentScore.score === 0 ?
-              <h3>happy</h3>
-              :
-              sentimentScore.score > 0 ?
-                <h3>So haapy</h3>
-                :
-                <h3>not happy</h3>
-            : ''
+          data.map((user) => setPhrase(user.text));
         }
+      }
+    });
+  };
 
-        </div>
+  const photoUrl = values.user._id
+    ? `/api/users/photo/${values.user._id}?${new Date().getTime()}`
+    : "/api/users/defaultphoto";
+  if (values.redirectToSignin) {
+    return <Redirect to="/signin" />;
+  }
+  const [phrase, setPhrase] = useState("");
+  const [sentimentScore, setSentimentScore] = useState("");
 
-        <ProfileTabs user={values.user} posts={posts} />
-{/* <PostList user={values.user} removeUpdate={props.removePostUpdate} posts={props.posts}/> */}
-      </Paper>
-    )
+  useEffect(() => {
+    setSentimentScore(sentiment.analyze(phrase));
+  }, [phrase]);
+  console.log(sentimentScore);
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <div>
+        {sentimentScore !== null ? (
+          <h1>Sentiment Analysis Score: {sentimentScore.score}</h1>
+        ) : (
+          ""
+        )}
+
+        {sentimentScore ? (
+          sentimentScore.score === 0 ? (
+            <h3>happy</h3>
+          ) : sentimentScore.score > 0 ? (
+            <h3>So haapy</h3>
+          ) : (
+            <h3>not happy</h3>
+          )
+        ) : (
+          ""
+        )}
+      </div>
+
+      <ProfileTabs user={values.user} posts={posts} score={true} />
+      {/* <PostList user={values.user} removeUpdate={props.removePostUpdate} posts={props.posts}/> */}
+    </Paper>
+  );
 }
-
-
